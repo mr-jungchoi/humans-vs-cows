183e585c721536d6e9b82dd8f2af885d6b25eb17
Jung Choi <jung.choi-i@plated.com>
2017-06-12 19:11:40 -0400

    Create navbar with react

---
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='/'>Humans VS. Cows</a>
+      </div>
+    </nav>
+  )
+}
