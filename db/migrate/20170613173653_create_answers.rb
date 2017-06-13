d960930b933b8b859999fcb4add4f3b62df520bb
Jung Choi <jung.choi-i@plated.com>
2017-06-13 13:41:57 -0400

    Add join table for choices and questions

--- db/migrate/20170613173653_create_answers.rb
+++ db/migrate/20170613173653_create_answers.rb
@@ -1,10 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.boolean  :is_correct, null: false
       t.references :survey_round, index: true, foreign_key: true
-      t.references :question, index: true, foreign_key: true
-      t.references :choice, index: true, foreign_key: true
+      t.references :choices_question, foreign_key: true
 
       t.timestamps null: false
     end
