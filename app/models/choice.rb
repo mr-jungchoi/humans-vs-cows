569cc03c8b1eaf24ef11577fbad67fe510c33747
Jung Choi <jung.choi-i@plated.com>
2017-06-13 15:06:30 -0400

    Create seed data

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,4 @@
-class Choice < ActiveRecord::Base
+class Choice < ApplicationRecord
   has_many :choices_questions
   has_many :questions, through: :choices_questions
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
-class Question < ActiveRecord::Base
+class Question < ApplicationRecord
   has_many :choices_questions
   has_many :choices, through: :choices_questions
 end
