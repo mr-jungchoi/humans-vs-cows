c10959bc43e634704be5bcb8536dc029d58b18d6
Jung Choi <jung.choi-i@plated.com>
2017-06-15 15:03:46 -0400

    Update admin link

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -3,7 +3,7 @@ function NavBar() {
     <nav className='navbar'>
       <div className='container-fluid'>
         <a href='/' className='navbar-brand'>Humans VS. Cows</a>
-        <a href='/admin/login' className='navbar-nav navbar-right btn btn-lg'>
+        <a href='/admin' className='navbar-nav navbar-right btn btn-lg'>
           <span className='glyphicon glyphicon-cog' aria-hidden='true'></span>
         </a>
       </div>
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,10 @@
 class AdminController < ApplicationController
   def index
     # Data should follow the format:
-    # {questions:
+    # [
     #   {id: 1, text: "string", choices: [{id: 1, text: "string"}, {..}]},
     #   {..}
-    # }
+    # ]
     @questions = []
     Question.all.each do |question|
       @questions.push({id: question.id, text: question.text, choices: question.choices})
